b5ffb746669077a7d2a179125bbaab876257c416
Wade Cosby <wadecosby@wades-mbp.attlocal.net>
2018-07-09 22:56:01 -0500

    corrects a typo on last commit from variable to string

--- src/state.js
+++ src/state.js
@@ -2,12 +2,28 @@ export default {
   contact:[
       {
         icon: 'faLinkedin',
-        name: LinkedIn
+        name: 'LinkedIn'
       },
     ],
   about:[
     {
+      link: 'Wade Cosby',
       title: 'About Me',
+      description: ''
+    },
+    {
+      link: 'Projects',
+      title: 'About Me',
+      description: ''
+    },
+    {
+      link: 'Resume',
+      title: 'Qualifications and Skills',
+      description: ''
+    },
+    {
+      link: 'Contact',
+      title: 'Ways to Connect',
       description: ''
     },
   ],
